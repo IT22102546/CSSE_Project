b21dbca8f63ed5ffb0d610241064535c5dd188e5
Sanjana Nimesh <sanjana.nim2001@gmail.com>
2024-07-26 13:36:38 +0530

    Frontend Backend connection created

--- api/index.js
+++ api/index.js
@@ -18,3 +18,19 @@ app.use(express.json());
 app.listen(3000,()=>{
     console.log("Server is Running on Port 3000");
 });
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+};
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode
+    });
+})
+
