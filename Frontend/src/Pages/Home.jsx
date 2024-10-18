969ea7d5a17bf46447e3fdacfd5e9fc35b0d4c94
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-10-18 11:52:34 +0530

    feat:mapbox implemented

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import WasteForm from '../Components/WasteForm'
+import WasteCollectorInterface from '../Components/DriverMap'
 
 export default function Home() {
   return (
     <div>
       <WasteForm/>
+      <div>
+        driver map
+        <div>
+          <WasteCollectorInterface/>
+        </div>
+      </div>
     </div>
   )
 }
